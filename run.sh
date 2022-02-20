f74741c65b3adebb07434053bdc929d700b50b2c
Dom Slee <domslee1@gmail.com>
2022-02-20 22:38:21 +1100

    renames

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ make -j;
 
 # use -r for profiling
 # 989
-LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libprofiler.so env CPUPROFILE=out11.prof \
+LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libprofiler.so env CPUPROFILE=./prof/out.prof \
 ./bin/solve --max-tries 4 --max-incorrect 146 -r ext/wordle-guesses.txt ext/wordle-answers.txt
