1101edc0fb1bca11a756e73b6ec1ea98cf3aa769
Dom Slee <domslee1@gmail.com>
2022-09-11 01:22:30 +1000

    Add test

---
+++ models/out.res
@@ -0,0 +1,4 @@
+maxWrong: 0
+word,numWrong,numTries
+lanes,1,0
+lears,1,0
--- test/IntegrationTest.h
+++ test/IntegrationTest.h
@@ -30,7 +30,7 @@ private:
 };
 
 
-TEST_CASE("tenor,raced should be 17") {
+TEST_CASE("G4 tenor,raced should be 17") {
     //  ./bin/solve -Seartol -p -I20 -g4 ext/wordle-guesses.txt ext/wordle-answers.txt
     auto argHelper = ArgHelper({"solve", "-Seartolsinc",  "-I20", "-g4", "--guesses", "ext/wordle-guesses.txt", "--answers", "ext/wordle-answers.txt", "--guesses-to-check", "test/TenorRaced.txt"});
     auto [argc, argv] = argHelper.getArgcArgv();
@@ -44,19 +44,20 @@ TEST_CASE("tenor,raced should be 17") {
     REQUIRE_MESSAGE(pairs[2].numWrong == 21, 21);
 }
 
-TEST_CASE("medium") {
-    auto argHelper = ArgHelper({"solve", "-Seartolsinc", "-I0", "-g5", "--guesses", "ext/wordle-guesses.txt", "--answers", "ext/wordle-answers.txt", "--guesses-to-check", "test/LanesLears.txt"});
+TEST_CASE("G5 roate,soare,orate") {
+    auto argHelper = ArgHelper({"solve", "-Seartolsinc", "-I0", "-g5", "--guesses", "ext/wordle-guesses.txt", "--answers", "ext/wordle-answers.txt", "--guesses-to-check", "test/RoateSoareOrate.txt", "--m-partitions-rem-depth", "0", "--M-partitions-rem-depth", "500"});
     auto [argc, argv] = argHelper.getArgcArgv();
     parseArgs(argc, argv);
     auto r = Runner::run();
     REQUIRE_MESSAGE(r.size() == 1, "ONE RESULT");
     const auto &pairs = r[0].pairs;
-    REQUIRE_MESSAGE(pairs.size() == 2, "NUM PAIRS");
+    REQUIRE_MESSAGE(pairs.size() == 3, "NUM PAIRS");
     REQUIRE_MESSAGE(pairs[0].numWrong == 0, 0);
     REQUIRE_MESSAGE(pairs[1].numWrong == 0, 0);
+    REQUIRE_MESSAGE(pairs[2].numWrong == 0, 0);
 }
 
-TEST_CASE("BIGHIDDEN lanes,lears") {
+TEST_CASE("G6 BIGHIDDEN lanes,lears") {
     if (std::getenv("IGNORE_BIGTESTS")) {
         DEBUG("ignoring big test...");
         return;
